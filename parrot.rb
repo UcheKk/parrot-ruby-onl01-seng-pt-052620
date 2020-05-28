1d7e5c0cca2e9f2c162d9c3acb49b5ba8941d687
Uche Nweke Jr <unwekejr@gmail.com>
2020-05-28 23:11:31 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot_noise(phrase="Squawk!")
-puts phrase
-return phrase
-2.6.1 :004?>   end
+  puts phrase
+  return phrase
+end
